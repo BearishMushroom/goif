50f124adda24613513599136b4047065ba424de9
Přemek Vyhnal <premek@users.noreply.github.com>
2016-09-05 00:36:33 +0200

    typo (#1)

--- goif.lua
+++ goif.lua
@@ -47,7 +47,7 @@ end
 
 goif.start = function(width, height)
   width = width or love.graphics.getWidth()
-  height = height or love.graphics.getHeght()
+  height = height or love.graphics.getHeight()
   goif.frame = 0
   goif.data = {}
   goif.active = true
